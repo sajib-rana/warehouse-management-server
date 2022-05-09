64a6a9ec623318a6ebdc9562af14540a33be28ed
sajib-rana <sajibrana444@gmail.com>
2022-05-09 10:47:26 +0600

    insert data and connected

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ss0vn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log('warehouse connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res)=>{
     res.send('Running warehouse server')
